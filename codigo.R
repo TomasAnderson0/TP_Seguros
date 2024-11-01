1d20ce79146b2cb7d82b2d18a1cdbfc277031f9c
Tomas <tomas.anderson508375@gmail.com>
2024-10-31 22:12:19 -0300

    Primeras ideas

--- codigo.R
+++ codigo.R
@@ -5,7 +5,40 @@ library(tidyverse)
 datos <- read_excel("Base de datos_TP_Seguros.xlsx")[,c(1,4)] %>% `colnames<-`(c("tiempo", "cuantia"))
 
 
+parametros = datos %>% group_by(tiempo) %>% count() %>% ungroup(tiempo) %>%  summarise(promedio = mean(n),
+                                                                                       variancia = var(n))
+                                                                                       
+datos %>% group_by(tiempo) %>% count() %>% ungroup(tiempo) %>% 
+  ggplot() + geom_point(aes(x = tiempo, y = n))
+
+
+# Binomial negativa
+
+lambda = parametros$promedio[1]
+h = lambda^2 / (parametros$variancia[1]-lambda) 
+p = h / (h + lambda)
+q = 1 - p
+
+valor = numeric()
+valor[1] = p ^ h 
+for (i in 1:50) {
+  valor[i+1] =(h + i - 1) * valor[i] * q / i
+}
+
+plot(valor)
+
+
+datos %>% group_by(tiempo) %>% count() %>% ungroup(tiempo) %>% 
+  ggplot() + geom_histogram(aes(x = n))
+
 
 ggplot(datos) + geom_histogram(aes(x = cuantia))
+
+
+
+
+
+
+
 
 
