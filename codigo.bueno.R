d178b6d8fa711d75b89feee7e043578cca293385
TomasAnderson0 <tomas.anderson508375@gmail.com>
2024-11-26 12:37:18 -0300

    finalizando

--- codigo.bueno.R
+++ codigo.bueno.R
@@ -7,35 +7,19 @@ library(ExtDist)
 datos <- read_excel("Base de datos_TP_Seguros.xlsx")[,c(1,4)] %>% `colnames<-`(c("tiempo", "cuantia"))
 datos = datos %>% mutate(cuantia = cuantia / 10^6)
 
-parametros = datos %>% group_by(tiempo) %>%
-             count() %>% ungroup(tiempo) %>%  
-             summarise(promedio = mean(n), variancia = var(n))
-
-datos %>% group_by(tiempo) %>% count() %>% ungroup(tiempo) %>% 
-  ggplot() + geom_point(aes(x = tiempo, y = n))
-
-
-ggplot(datos) + geom_histogram(aes(x = cuantia))
-
 options(scipen = 999)
 
-ggplot(datos) + geom_point(aes(x = tiempo, y = cuantia))
-
-ggplot(datos) + geom_density(aes(x = cuantia))
 
 # Simulacion
 
-8/3431
 ## Poisson 
 lambda_poisson = 25615*((1/2) * nrow(datos)/25615 +  (1/6) * 3.023 /24.752 + (2/6) *  3.581 / 25.348)
-
-
-
 
 ## Weibull
 
 parametros_wei = eweibull(datos$cuantia)$parameters
 
+set.seed(2024)
 
 resultado = numeric()
 for (j in 1:1000) {
@@ -57,6 +41,8 @@ sum(datos$cuantia)
 quantile(resultado)
 (quantile(resultado, probs = .99) - median(resultado))
 
+source("sampling.R")
+set.seed(2024)
 resultado_sampling = numeric()
 for (j in 1:1000) {
   
@@ -73,3 +59,8 @@ for (j in 1:1000) {
 }
 quantile(resultado_sampling)
 (quantile(resultado_sampling, probs = .99) - median(resultado_sampling))
+
+save(resultado, resultado_sampling, file = "resultados.RData")
+
+
+
